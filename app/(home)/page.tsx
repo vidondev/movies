36c37a355951ceecf6b5959970cb39605abc997e
vidondev <vidon.dev@gmail.com>
2024-09-22 10:52:35 +0800

    update ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,52 +1,76 @@
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
-
-    <div className="space-y-4 p-4">
+    <div className="space-y-4 p-4 ">
       <div className="space-y-2">
         <h2>Now Playing</h2>
-        <Carousel className="w-full ">
-      <CarouselContent >
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className=" lg:basis-1/5">
-            <div className=" h-[400px]  border rounded-md  flex items-center justify-center text-3xl">{index}</div>
-            </CarouselItem>))}
-      </CarouselContent>
-      </Carousel>
+        <Carousel className="grid" opts={{
+          slidesToScroll: 'auto',
+        }}>
+          <CarouselContent>
+            {Array.from({ length: 20 }).map((_, index) => (
+              <CarouselItem key={index} className="menu-item">
+                <div className=" h-[150px]  border rounded-md  flex items-center justify-center text-3xl">
+                  {index}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
-      <div className="space-y-2 ">
+      <div className="space-y-2">
         <h2>Now Playing</h2>
-        <Carousel className="w-full ">
-      <CarouselContent >
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className=" lg:basis-1/5">
-            <div className=" h-[400px]  border rounded-md  flex items-center justify-center text-3xl">{index}</div>
-            </CarouselItem>))}
-      </CarouselContent>
-      </Carousel>
+        <Carousel className="grid" opts={{
+          slidesToScroll: 'auto',
+        }}>
+          <CarouselContent>
+            {Array.from({ length: 20 }).map((_, index) => (
+              <CarouselItem key={index} className="menu-item">
+                <div className=" h-[150px]  border rounded-md  flex items-center justify-center text-3xl">
+                  {index}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div><div className="space-y-2">
+        <h2>Now Playing</h2>
+        <Carousel className="grid" opts={{
+          slidesToScroll: 'auto',
+        }}>
+          <CarouselContent>
+            {Array.from({ length: 20 }).map((_, index) => (
+              <CarouselItem key={index} className="menu-item">
+                <div className=" h-[150px]  border rounded-md  flex items-center justify-center text-3xl">
+                  {index}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
-      <div className="space-y-2 ">
+      <div className="space-y-2">
         <h2>Now Playing</h2>
-        <Carousel className="w-full ">
-      <CarouselContent >
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className=" lg:basis-1/5">
-            <div className=" h-[400px]  border rounded-md  flex items-center justify-center text-3xl">{index}</div>
-            </CarouselItem>))}
-      </CarouselContent>
-      </Carousel>
-      </div><div className="space-y-2 ">
-        <h2>Now Playing</h2>
-        <Carousel className="w-full ">
-      <CarouselContent >
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className=" lg:basis-1/5">
-            <div className=" h-[400px]  border rounded-md  flex items-center justify-center text-3xl">{index}</div>
-            </CarouselItem>))}
-      </CarouselContent>
-      </Carousel>
+        <Carousel className="grid" opts={{
+          slidesToScroll: 'auto',
+        }}>
+          <CarouselContent>
+            {Array.from({ length: 20 }).map((_, index) => (
+              <CarouselItem key={index} className="menu-item">
+                <div className=" h-[150px]  border rounded-md  flex items-center justify-center text-3xl">
+                  {index}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
     </div>
   );
